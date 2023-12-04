0773d15735bcaa8993f9d3be42b871cb6c253b54
Eric Promislow <eric.promislow@suse.com>
2023-12-03 19:42:57 -0800

    Add a test for win-loss end-game situations.

--- test/integration/first_winner_test.rb
+++ test/integration/first_winner_test.rb
@@ -121,4 +121,44 @@ class FirstWinnerTest < ActionDispatch::IntegrationTest
     ]
     verify_previous_perturbed_guesses(@user1.username, expected)
   end
+
+  test "user1 still not right, and user2 is correct so they go to states 9 and 8 resp" do
+    log_in_as(@user1)
+    post guesses_path, params: {game_state_id: @gs1.id, word:"fleck" }
+    assert_redirected_to game_path(@game)
+    follow_redirect!
+    assert_template 'games/_show3'
+    assert_select "p", "Waiting for #{ @user2.username } to finish guessing a word."
+
+    log_in_as(@user2)
+    post guesses_path, params: {game_state_id: @gs2.id, word:"madam" }
+    assert_redirected_to game_path(@game)
+    follow_redirect!
+    assert_template 'games/_show8'
+    assert_select "h1", "Congratulations, you won!"
+    assert_select "span#result1", "You got it in 4 guesses!"
+    assert_select "button#shareResults", "Copy to Clipboard"
+    # Targeting madam
+    expected = [
+      { word: "triad", score: "0:0:0:2:1", liePosition: 0, lieColor: 2, actualColor: 0 },
+      { word: "tonal", score: "0:0:0:2:0", liePosition: 4, lieColor: 2, actualColor: 0 },
+      { word: "tidal", score: "0:0:2:2:0", liePosition: 3, lieColor: 0, actualColor: 2 },
+      { word: "madam", score: "2:2:2:2:2", isCorrect: true },
+    ]
+    verify_previous_perturbed_guesses(@user2.username, expected)
+
+    log_in_as(@user1)
+    get game_path(@game)
+    assert_template 'games/_show9'
+    assert_select "h1", "Sorry, you lost to #{ @user2.username }"
+    assert_select "h2", "Your word was #{ @gs2.finalWord }"
+    # targeting "block"
+    expected = [
+      { word: "space", score: "0:0:0:2:0", liePosition: 4, lieColor: 1, actualColor: 0 },
+      { word: "relic", score: "0:0:1:0:1", liePosition: 2, lieColor: 0, actualColor: 1 },
+      { word: "deuce", score: "0:0:0:2:0", liePosition: 2, lieColor: 2, actualColor: 0 },
+      { word: "fleck", score: "0:2:0:2:2", isCorrect: false },
+    ]
+    verify_previous_perturbed_guesses(@user1.username, expected)
+  end
 end
