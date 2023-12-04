47c08952918a7d846b7d7eb3dba59c7cd1689e18
Eric Promislow <eric.promislow@suse.com>
2023-12-03 19:32:41 -0800

    Add and test game-states for first-player winning.

--- test/integration/first_winner_test.rb
+++ test/integration/first_winner_test.rb
@@ -71,4 +71,54 @@ class FirstWinnerTest < ActionDispatch::IntegrationTest
     ]
     verify_previous_perturbed_guesses(@user1.username, expected)
   end
+
+  test "user1 with correct word moves to template 6 and user2 is wrong so they go to states 8 and 9 resp" do
+    log_in_as(@user1)
+    post guesses_path, params: {game_state_id: @gs1.id, word:"block" }
+    assert_redirected_to game_path(@game)
+    follow_redirect!
+    assert_template 'games/_show6'
+    assert_select "h1", "You're a non-loser!"
+    assert_select "span#result1", "You got it in 4 guesses!"
+    assert_select "button#shareResults", "Copy to Clipboard"
+    assert_select "p", "Waiting to see if #{ @user2.username } gets it this turn..."
+    # targeting "block"
+    expected = [
+      { word: "space", score: "0:0:0:2:0", liePosition: 4, lieColor: 1, actualColor: 0 },
+      { word: "relic", score: "0:0:1:0:1", liePosition: 2, lieColor: 0, actualColor: 1 },
+      { word: "deuce", score: "0:0:0:2:0", liePosition: 2, lieColor: 2, actualColor: 0 },
+      { word: "block", score: "2:2:2:2:2", isCorrect: true },
+    ]
+    verify_previous_perturbed_guesses(@user1.username, expected)
+
+    log_in_as(@user2)
+    post guesses_path, params: {game_state_id: @gs2.id, word:"medal" }
+    assert_redirected_to game_path(@game)
+    follow_redirect!
+    assert_template 'games/_show9'
+    assert_select "h1", "Sorry, you lost to #{ @user1.username }"
+    assert_select "h2", "Your word was #{ @gs1.finalWord }"
+    # Targeting madam
+    expected = [
+      { word: "triad", score: "0:0:0:2:1", liePosition: 0, lieColor: 2, actualColor: 0 },
+      { word: "tonal", score: "0:0:0:2:0", liePosition: 4, lieColor: 2, actualColor: 0 },
+      { word: "tidal", score: "0:0:2:2:0", liePosition: 3, lieColor: 0, actualColor: 2 },
+      { word: "medal", score: "2:0:2:2:0", isCorrect: false },
+    ]
+    verify_previous_perturbed_guesses(@user2.username, expected)
+
+    log_in_as(@user1)
+    get game_path(@game)
+    assert_template 'games/_show8'
+    assert_select "h1", "Congratulations, you won!"
+    assert_select "span#result1", "You got it in 4 guesses!"
+    assert_select "button#shareResults", "Copy to Clipboard"
+    expected = [
+      { word: "space", score: "0:0:0:2:0", liePosition: 4, lieColor: 1, actualColor: 0 },
+      { word: "relic", score: "0:0:1:0:1", liePosition: 2, lieColor: 0, actualColor: 1 },
+      { word: "deuce", score: "0:0:0:2:0", liePosition: 2, lieColor: 2, actualColor: 0 },
+      { word: "block", score: "2:2:2:2:2", isCorrect: true },
+    ]
+    verify_previous_perturbed_guesses(@user1.username, expected)
+  end
 end
